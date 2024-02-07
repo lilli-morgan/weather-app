3ff75115878b43f45edfdd983d7573f3ff7aa919
lilli-morgan <lilliconstance@gmail.com>
2024-02-07 19:59:12 +1100

    set up basic weather integration

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+function App() {
+  // two state variables - query = stores user's search query & weather = stores weather data from API
+  const [location, setLocation] = useState("");
+  const [weather, setWeather] = useState(null);
+
+  const API_KEY = "4df4b82b5d4ff0d82caae1a4891f9ce1";
+
+  const fetchWeatherData = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`
+      );
+      // Change the state (sets weather as returned data)
+      setWeather(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div className="App">
+      <input
+        className="search-bar"
+        type="text"
+        placeholder="Enter a location"
+        onChange={(event) => setLocation(event.target.value)}
+      ></input>
+      <button type="submit" onClick={fetchWeatherData}>
+        Get Weather
+      </button>
+
+      {weather && (
+        <div>
+          <h2>{weather.name}</h2>
+          <p>{weather.weather[0].description}</p>
+          <p>Temperature: {weather.main.temp}</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
